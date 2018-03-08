e4a4da4ebae463f44491ef103c692399f2591db1
Olle Lauri Boström <ollebostr@gmail.com>
2018-03-08 14:39:47 +0100

    Refactoring login route

--- src/api/login.js
+++ src/api/login.js
@@ -5,25 +5,27 @@ export default function (config) {
   const router = Router();
 
   router.get('/', (req, res) => {
-    const url = `${config.ghLoginUrl}?client_id=${config.ghClientID}&redirect_url=${config.ghCallbackURL}&scope=admin:org,repo,user&state=${Math.random().toString(36).substring(7)}`;
+    const base = config.ghLoginUrl;
+    const id = config.ghClientID;
+    const redirect = config.ghCallbackURL;
+    const state = Math.random().toString(36).substring(7);
+    const scope = 'admin:org,repo,user';
+    const url = `${base}?client_id=${id}&redirect_url=${redirect}&scope=${scope}&state=${state}`;
     res.redirect(url);
   });
 
   router.get('/callback', async (req, res, next) => {
     try {
+      const headers = { headers: { Accept: 'application/json' } };
       const tokenRequest = await axios.post(config.ghTokenUrl, {
         client_id: config.ghClientID,
         client_secret: config.ghClientSecret,
         code: req.query.code,
         state: req.query.state,
-      }, {
-        headers: {
-          Accept: 'application/json',
-        },
-      });
+      }, headers);
       res.redirect(`${config.tokenRedirect}?token=${tokenRequest.data.access_token}`);
     } catch (err) {
-      next(err);
+      res.redirect(`${config.tokenRedirect}?err=${err.message}`);
     }
   });
 
